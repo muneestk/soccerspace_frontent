e2d537c192b9bff79d153997c106796d538340d4
muneestk <125794679+muneestk@users.noreply.github.com>
2023-10-27 12:30:55 +0400

    Untrack src/environments directory

--- src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production:false,
-    API_Key : "http://localhost:3000/admin" ,
-    User_API_Key : "http://localhost:3000",
-    Manager_API_Key : "http://localhost:3000/manager"
-};
-
