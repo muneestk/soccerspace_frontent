6dcf6bbdc819a41ac11962a2ef5e45b5f44a7a78
muneestk <125794679+muneestk@users.noreply.github.com>
2023-11-09 11:42:27 +0400

    nogitignore

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production:false,
+    NEXT_PUBLIC_API_Key : "http://localhost:3000/admin" ,
+    NEXT_PUBLIC_User_API_Key : "http://localhost:3000",
+    NEXT_PUBLIC_Manager_API_Key : "http://localhost:3000/manager"
+};
+
