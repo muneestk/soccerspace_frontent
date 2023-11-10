221664a720042c2b7f3c1c8de70abe21e9200c2d
muneestk <125794679+muneestk@users.noreply.github.com>
2023-11-10 11:27:33 +0400

    aws

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production:false,
-    NEXT_PUBLIC_API_Key : "https://soccerspace-server.onrender.com/admin" ,
-    NEXT_PUBLIC_User_API_Key : "https://soccerspace-server.onrender.com",
-    NEXT_PUBLIC_Manager_API_Key : "https://soccerspace-server.onrender.com/manager",
+    NEXT_PUBLIC_API_Key : "http://65.1.241.237/admin" ,
+    NEXT_PUBLIC_User_API_Key : "http://65.1.241.237",
+    NEXT_PUBLIC_Manager_API_Key : "http://65.1.241.237/manager",
     NEXT_PUBLIC_Google_API_Key : "80902647843-4966kmts5ceeckm6h4rqdkn6bmctef58.apps.googleusercontent.com"
 };
 
