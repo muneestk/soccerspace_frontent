2a28a5ea769b797484ee5f72aae78a404e172266
muneestk <125794679+muneestk@users.noreply.github.com>
2023-11-11 11:35:12 +0400

    graph

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,8 @@
-
 export const environment = {
     production:false,
-    NEXT_PUBLIC_API_Key : "http://localhost:3000/admin" ,
-    NEXT_PUBLIC_User_API_Key : "http://localhost:3000",
-    NEXT_PUBLIC_Manager_API_Key : "http://localhost:3000/manager",
+    NEXT_PUBLIC_API_Key : "https://essense.site/admin" ,
+    NEXT_PUBLIC_User_API_Key : "https://essense.site",
+    NEXT_PUBLIC_Manager_API_Key : "https://essense.site/manager",
     NEXT_PUBLIC_Google_API_Key : "80902647843-4966kmts5ceeckm6h4rqdkn6bmctef58.apps.googleusercontent.com"
 };
 
